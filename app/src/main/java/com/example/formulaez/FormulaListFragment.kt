eabf1fcaa023d0cdd20da10670673b45690309b5
Aske <aske.lorenz@gm,ail.com>
2021-05-23 18:08:39 +0200

    Nu er der rigtige formler

--- app/src/main/java/com/example/formulaez/FormulaListFragment.kt
+++ app/src/main/java/com/example/formulaez/FormulaListFragment.kt
@@ -15,10 +15,10 @@ class FormulaListFragment : ListFragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // We need to use a different list item layout for devices older than Honeycomb
+
         val layout = android.R.layout.simple_list_item_activated_1
 
-        // Create an array adapter for the list view, using the Ipsum headlines array
+
         listAdapter = ArrayAdapter(requireActivity(), layout, formulaViewModel.getFormulaHeadlines())
         }
 
@@ -26,21 +26,18 @@ class FormulaListFragment : ListFragment() {
     override fun onStart() {
         super.onStart()
 
-        // When in two-pane layout, set the listview to highlight the selected list item
-        // (We do this during onStart because at the point the listview is available.)
-        // This is for when it is in big mode so needs to be implemented later
+
         if (parentFragmentManager.findFragmentById(R.id.description_fragment) != null) {
            listView.choiceMode = ListView.CHOICE_MODE_SINGLE
         }
     }
 
     override fun onListItemClick(l: ListView, v: View, position: Int, id: Long) {
-        // Notify the parent activity of selected item
-        //mCallback?.onArticleSelected(position)
 
-        // Set the item as checked to be highlighted when in two-pane layout
+
+
         formulaViewModel.setClickedFormula(position)
         listView.setItemChecked(position, true)
-        //listView.setItemChecked(position, true)
+
     }
 }
--- app/src/main/java/models/FormulaManager.kt
+++ app/src/main/java/models/FormulaManager.kt
@@ -4,7 +4,11 @@ import android.util.Log
 
 
 class FormulaManager(repo: FormulaRepository) {
-
+    val nameArray: Array<String> = arrayOf("Kapitalformel","Annuitetsopsparing","Kvadratsætning 1", "Kvadratsætning 2", "Kvadratsætning 3", "Phytagoras")
+    val descriptionArray: Array<String> = arrayOf("Startkapital K0 \n Rente p% pr. termin  \n Kapital K efter n terminer","Terminsindbetaling b \n Rentefod r  \n Antal indbetalinger n  \n Kapital A efter sidste indbetaling"
+    , "Kvadratsætning", "Kvadratsætninger", "Kvadratsætninger", "Find længden af hypotanusen")
+    val categoryArray: Array<String> = arrayOf("Økonomi", "Økonomi", "Matematik", "Matematik", "Matematik", "Trigonometri")
+    val mathjaxArray: Array<String> = arrayOf("K = k0 * (1+r)^n", "A=b*(((1+r)^n-1)/r)","(a+b)^2 = a^2 + b^2 + 2a*b","(a-b)^2 = a^2 + b^2 - 2a*b","(a+b)*(a-b) = a^2 - b^2", "c^2 = a^2 + b^2")
     private lateinit var formulas: List<Formula>
     var repository = repo
 
@@ -12,17 +16,18 @@ class FormulaManager(repo: FormulaRepository) {
         Log.d("LOGGER", "init : FormulaManager")
 
         if(repository.formulas.isEmpty()){makeFormulaDatabase()}
+
         makeFormulaList()
     }
 
     private fun makeFormulaDatabase(){
-       for (i in 1..10){
+       for (i in 0..5){
            var formula = Formula()
-           //formula.uid = i
-           formula.name = "Name" + i
-           formula.category = "Category"
-           formula.description = "Description"
-           formula.mathJaxForm = "Mathjax"
+
+           formula.name = nameArray[i]
+           formula.category = categoryArray[i]
+           formula.description = descriptionArray[i]
+           formula.mathJaxForm = mathjaxArray[i]
            formula.form = "Form"
            repository.insert(formula)
         }
@@ -35,7 +40,7 @@ class FormulaManager(repo: FormulaRepository) {
     fun getFormulaHeadlines(): Array<String?> {
         val formulaHeadlines = arrayOfNulls<String>(formulas.size)
 
-        formulas.forEachIndexed{i, formula -> formulaHeadlines[i] = formula.name + " " + formula.category + " " + formula.mathJaxForm}
+        formulas.forEachIndexed{i, formula -> formulaHeadlines[i] = "Formula: " + formula.name + "\n" + "Category: " +formula.category + "\n" + formula.mathJaxForm}
 
         return formulaHeadlines
     }
