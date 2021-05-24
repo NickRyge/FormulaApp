3a8f9e0d2de1ee8fc7e970d009b0e5bd6435e9f2
Aske <aske.lorenz@gm,ail.com>
2021-05-24 17:25:24 +0200

    search

--- app/src/main/java/com/example/formulaez/MainActivity.kt
+++ app/src/main/java/com/example/formulaez/MainActivity.kt
@@ -74,11 +74,11 @@ class MainActivity : AppCompatActivity() {
         var edit = findViewById<EditText>(R.id.input)
         edit.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                TODO("Not yet implemented")
+                return
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                TODO("Not yet implemented")
+                return
             }
 
             override fun afterTextChanged(s: Editable?) {
